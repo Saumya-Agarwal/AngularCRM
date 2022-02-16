db02f89cd8ae90aab4d56bd22d6282a071406907
Mohammed Alam <mohammedalamkhan6@gmail.com>
2022-02-16 14:33:39 +0530

    first commit

---
+++ src/app/features/after-login/after-login.component.spec.ts
@@ -0,0 +1,31 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ScamComponent } from '@shared/components/scam/scam.component';
+import { SharedModule } from '../../shared/shared.module';
+
+import { AfterLoginComponent } from './after-login.component';
+
+describe('AfterLoginComponent', () => {
+  let component: AfterLoginComponent;
+  let fixture: ComponentFixture<AfterLoginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AfterLoginComponent, ScamComponent],
+      imports: [SharedModule, MatDatepickerModule, MatNativeDateModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AfterLoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
